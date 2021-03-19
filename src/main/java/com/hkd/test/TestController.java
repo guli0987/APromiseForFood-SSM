d8e937646fd3bd3f2d0ead7405e18c9951c423da
guli0987 <52617482+guli0987@users.noreply.github.com>
2021-03-19 17:38:39 +0800

    项目结构调整

--- src/main/java/com/hkd/test/TestController.java
+++ src/main/java/com/hkd/test/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestController {
+
     public static void main(String[] args) {
         String xmlPath = "applicationContext.xml";
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
